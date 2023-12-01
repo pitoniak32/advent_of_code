d7f6180a24816a29d7b5eb7cc807f80822e4b5b1
David Pitoniak <davidpitoniak32@gmail.com>
2023-12-01 08:20:44 -0500

    day-01: finish part1 and start part2

--- 2023/day-01/src/bin/part1.rs
+++ 2023/day-01/src/bin/part1.rs
@@ -3,16 +3,51 @@ use anyhow::Result;
 fn main() -> Result<()> {
     let input_file = include_str!("../../input1.txt");
     println!("{}", input_file);
+    let mut total_code = 0;
+    for line in input_file.split("\n") {
+        if line.trim().len() < 1 {
+            continue
+        }
+        let code = get_code(line);
+        total_code += code;
+    }
+    println!("final code: {total_code}");
     Ok(())
+}
+
+fn get_code(code: &str) -> u32 {
+    let mut vals = vec![];
+    for l_char in code.chars() {
+        if l_char.is_digit(10) {
+            vals.push(l_char);
+        }
+    }
+
+    println!("{vals:?}");
+
+    if vals.len() == 1 {
+        vals.push(vals.get(0).unwrap().clone());
+    }
+
+    format!(
+        "{}{}",
+        vals.get(0).unwrap().clone(),
+        vals.get(vals.len() - 1).unwrap().clone(),
+    )
+    .parse::<u32>()
+    .expect("to be u32")
 }
 
 #[cfg(test)]
 mod tests {
-    use pretty_assertions::{assert_eq, assert_ne};
     use super::*;
+    use pretty_assertions::assert_eq;
 
     #[test]
-    fn test_it_works() {
-        assert_eq!(1, 1);
+    fn test_example_works() {
+        // Arrange
+        let expected = 15;
+        // Act / Assert
+        assert_eq!(get_code("a1b2c3d4e5f"), expected);
     }
 }
--- 2023/day-01/src/bin/part2.rs
+++ 2023/day-01/src/bin/part2.rs
@@ -3,16 +3,35 @@ use anyhow::Result;
 fn main() -> Result<()> {
     let input_file = include_str!("../../input2.txt");
     println!("{}", input_file);
+    let mut total_code = 0;
+    for line in input_file.split("\n") {
+        if line.trim().len() < 1 {
+            continue;
+        }
+        let code = get_code(line);
+        total_code += code;
+    }
+    println!("final code: {total_code}");
     Ok(())
+}
+
+// five1two = 52
+fn get_code(code: &str) -> u32 {
+    0
 }
 
 #[cfg(test)]
 mod tests {
-    use pretty_assertions::{assert_eq, assert_ne};
     use super::*;
+    use pretty_assertions::assert_eq;
 
     #[test]
-    fn test_it_works() {
-        assert_eq!(1, 1);
+    fn test_it_works_29() {
+        assert_eq!(get_code("two1nine"), 29);
+    }
+
+    #[test]
+    fn test_it_works_83() {
+        assert_eq!(get_code("eightwothree"), 83);
     }
 }
