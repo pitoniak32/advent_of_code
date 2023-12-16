34598b11d3ccf67a3f36ca2cbb0f383ccc730f76
David Pitoniak <davidpitoniak32@gmail.com>
2023-12-16 13:22:25 -0500

    day-14: finish part2

--- 2023/day-14/src/part2.rs
+++ 2023/day-14/src/part2.rs
@@ -1,4 +1,4 @@
-use std::fmt::Display;
+use std::{collections::HashMap, fmt::Display};
 
 use anyhow::Result;
 use util::{transpose_matrix};
@@ -37,27 +37,60 @@ impl From<char> for Tile {
     }
 }
 
+// 118786 = too high
+// 118780 = too high
+// 118747 = just right
 pub fn process(input: &str) -> Result<String> {
     let cycles = 1_000_000_000;
 
     let load = run_cycle(input, cycles);
 
-    Ok(load)
+    Ok(load.0)
 }
 
-pub fn run_cycle(input: &str, cycles: i32) -> String {
+pub fn run_cycle(input: &str, cycles: i32) -> (String, String) {
     let mut platform = input
         .lines()
         .map(|line| line.chars().map(Tile::from).collect())
         .collect::<Vec<Vec<_>>>();
 
-    // println!("pre---");
-    // print_matrix(&platform);
-    // println!("---");
-    //
-    let mut previous_cycle = platform.clone();
+    let mut cycle_cache: HashMap<String, (usize, Vec<Vec<Tile>>)> = HashMap::new();
+
+    let mut remaining_cycles = 0;
+    let mut cycle_count = 0;
 
     for i in 0..cycles {
+        let platform_string = platform
+            .iter()
+            .map(|row| row.iter().map(|c| c.to_char()).collect::<String>())
+            .collect::<Vec<String>>()
+            .join("\n");
+
+        if remaining_cycles == 0 {
+            if let Some(hit) = cycle_cache.get(&platform_string) {
+                let start_index = hit.0 as i32;
+                remaining_cycles = (1_000_000_000 - start_index) % (i - start_index);
+                println!(
+                    "cycle: {}, start: {}, period: {}",
+                    i, hit.0, remaining_cycles
+                );
+            } else {
+                cycle_cache.insert(platform_string, (i as usize, platform.clone()));
+            }
+        } else if cycle_count == remaining_cycles - 1 {
+            println!(
+                "i: {}, period: {}, count: {}",
+                i, remaining_cycles, cycle_count
+            );
+            break;
+        } else if cycle_count < remaining_cycles {
+            cycle_count += 1;
+            println!(
+                "i: {}, period: {}, count: {}",
+                i, remaining_cycles, cycle_count
+            );
+        }
+
         platform = transpose_matrix(&platform);
         platform = platform
             .iter_mut()
@@ -110,32 +143,23 @@ pub fn run_cycle(input: &str, cycles: i32) -> String {
                 row.clone()
             })
             .collect::<Vec<_>>();
-
-        if previous_cycle == platform {
-            break;
-        } else {
-            previous_cycle = platform.clone();
-        }
-
-        // println!("post-east---");
-        // print_matrix(&platform);
-        // println!("---");
-        if i % 1_000_000 == 0 {
-            println!("cycle: {i}");
-        }
     }
 
-    // let len = transposed.len() - 1;
-    // let load = transposed.iter().enumerate().fold(0, |mut acc, (i, row)| {
-    //     let stones = row.iter().filter(|c| c == &&Tile::O).count();
-    //     acc += (len - i + 1) * stones;
-    //     acc
-    // });
-    platform
-        .iter()
-        .map(|row| row.iter().map(|c| c.to_char()).collect::<String>())
-        .collect::<Vec<String>>()
-        .join("\n")
+    let len = platform.len();
+    let load = platform.iter().enumerate().fold(0, |mut acc, (i, row)| {
+        let stones = row.iter().filter(|c| c == &&Tile::O).count();
+        acc += (len - i) * stones;
+        acc
+    });
+
+    (
+        load.to_string(),
+        platform
+            .iter()
+            .map(|row| row.iter().map(|c| c.to_char()).collect::<String>())
+            .collect::<Vec<String>>()
+            .join("\n"),
+    )
 }
 
 #[cfg(test)]
@@ -200,8 +224,8 @@ O.#..O.#.#
         println!("---------");
         println!("input:\n{}\n---", input);
         println!("expected:\n{}\n---", expected);
-        println!("result:\n{}\n---", result);
-        assert_eq!(result, expected);
+        println!("result:\n{}\n---", result.1);
+        assert_eq!(result.1, expected);
         Ok(())
     }
 
