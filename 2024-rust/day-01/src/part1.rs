c25563736ca4ac870de1cc4bf093b473da0380f5
David Pitoniak <davidpitoniak32@gmail.com>
2024-12-01 09:54:29 -0500

    feat: finished day-01 part1 and 2 in rust

---
+++ 2024-rust/day-01/src/part1.rs
@@ -0,0 +1,56 @@
+use anyhow::Result;
+
+pub fn process(input: &str) -> Result<String> {
+    let (mut first_list, mut second_list) = input.lines().fold(
+        (Vec::<u32>::new(), Vec::<u32>::new()),
+        |(mut first_list, mut second_list), line| {
+            let parts = line.split("   ").collect::<Vec<_>>();
+            let first = parts.get(0).expect("input should have a first value");
+            let second = parts.get(1).expect("input should have a second value");
+            first_list.push(
+                first
+                    .parse::<u32>()
+                    .expect("input should be a valid integer"),
+            );
+            second_list.push(
+                second
+                    .parse::<u32>()
+                    .expect("input should be a valid integer"),
+            );
+            (first_list, second_list)
+        },
+    );
+
+    if first_list.len() != second_list.len() {
+        panic!("the lists must be the same length")
+    }
+
+    first_list.sort();
+    second_list.sort();
+
+    let result = first_list
+        .iter()
+        .zip(second_list)
+        .map(|(first, second)| first.abs_diff(second))
+        .sum::<u32>();
+
+    return Ok(result.to_string());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_process() -> Result<()> {
+        let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+        assert_eq!(process(input)?, "11");
+        Ok(())
+    }
+}
---
+++ 2024-rust/day-01/src/part2.rs
@@ -0,0 +1,61 @@
+use anyhow::Result;
+
+pub fn process(input: &str) -> Result<String> {
+    let (mut first_list, mut second_list) = input.lines().fold(
+        (Vec::<u32>::new(), Vec::<u32>::new()),
+        |(mut first_list, mut second_list), line| {
+            let parts = line.split("   ").collect::<Vec<_>>();
+            let first = parts.get(0).expect("input should have a first value");
+            let second = parts.get(1).expect("input should have a second value");
+            first_list.push(
+                first
+                    .parse::<u32>()
+                    .expect("input should be a valid integer"),
+            );
+            second_list.push(
+                second
+                    .parse::<u32>()
+                    .expect("input should be a valid integer"),
+            );
+            (first_list, second_list)
+        },
+    );
+
+    if first_list.len() != second_list.len() {
+        panic!("the lists must be the same length")
+    }
+
+    first_list.sort();
+    second_list.sort();
+
+    let mut total_similarity = 0;
+    for first_val in &first_list {
+        let mut curr_count = 0;
+        for sec_val in &second_list {
+            if first_val == sec_val {
+                curr_count += 1;
+            }
+        }
+        total_similarity += first_val * curr_count;
+    }
+
+    return Ok(total_similarity.to_string());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_process() -> Result<()> {
+        let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+        assert_eq!(process(input)?, "31");
+        Ok(())
+    }
+}
